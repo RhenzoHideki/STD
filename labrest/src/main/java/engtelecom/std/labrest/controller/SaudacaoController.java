fe0ff482095357f70d28e6a26cf7207016ee39f1
Rhenzo <RhenzoHideki@users.noreply.github.com>
2023-11-24 07:36:34 -0300

    Lab rest

--- labrest/src/main/java/engtelecom/std/labrest/controller/SaudacaoController.java
+++ labrest/src/main/java/engtelecom/std/labrest/controller/SaudacaoController.java
@@ -13,6 +13,11 @@ public class SaudacaoController {
     private static final String MENSAGEM = "Olá %s";
     private final AtomicLong contador = new AtomicLong();
 
+    @GetMapping("/ola")
+    public String olaMundo() {
+        return "Olá mundo";
+    }
+
     @GetMapping("/saudacao")
     public Saudacao saudacao(@RequestParam(value = "nome", defaultValue = "mundo") String nome) {
         return new Saudacao(contador.incrementAndGet(), String.format(MENSAGEM, nome));
